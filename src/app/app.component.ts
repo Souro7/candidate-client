4b1a6bf0e096016e9e9078912237c8dd5f3eaf98
Souro7 <sarkarsourobrata@gmail.com>
2021-04-25 17:54:43 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+class Candidate {
+  name: string;
+  id: string;
+  email: string;
+  appointmentDate: Date;
+  moveForward: boolean;
+  interviewTranscription: string;
+  actionResult: string;
+
+  constructor(json?: any) {
+    if (!json) {
+      return;
+    }
+    this.id = json['_id'] || '';
+    this.name = json['name'] || '';
+    this.email = json['email'] || '';
+    this.appointmentDate = new Date();
+    this.moveForward = json['move_forward'] || false;
+    this.interviewTranscription = json['interview_transcription'] || '';
+    this.actionResult = json['action_result'] || '';
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +30,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dashbaord';
+  candidates = [];
+  selectedCandidate = 0;
+  selectedDate: Date;
+
+  apiUrl = 'http://localhost:3000/api/candidates';
+
+  constructor(private http: HttpClient) {
+    this.getCandidates();
+  }
+
+  buildHeaders(argsObject: object): object {
+    if (!argsObject) {
+      // throw new Error('RequestOptionsArgs not passed to buildHeaders');
+    }
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    argsObject['headers'] = headers;
+    return argsObject;
+  }
+
+  getCandidates() {
+    this.http.get(this.apiUrl, this.buildHeaders({})).subscribe(response => {
+      console.log(response);
+      for (const candidate of response['data']) {
+        this.candidates.push(new Candidate(candidate));
+      }
+      console.log(this.candidates);
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  selectCandidate(id) {
+    this.selectedCandidate = id;
+    this.selectedDate = this.candidates[id]['appointmentDate'];
+  }
+
+  printDate(event) {
+    console.log(event.value);
+  }
 }
